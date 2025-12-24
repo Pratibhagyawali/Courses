a5d01c61c84c442ea6dec25ae88fd04c5e58e875
Pratibhagyawali <pratibhagyawali7@gmail.com>
2025-12-24 23:57:47 +0200

    Update A6_T1.py

--- week 6/A6_T1.py
+++ week 6/A6_T1.py
@@ -1,2 +1,16 @@
+print("Program starting.")
+print("This program can read a file.")
 
+filename = input("Insert filename: ")
+
+try:
+    with open(filename, "r") as f:
+        print(f'#### START "{filename}" ####')
+        for line in f:
+            print(line.rstrip())
+        print(f'#### END "{filename}" ####')
+except FileNotFoundError:
+    print(f'Error: File "{filename}" not found.')
+
+print("Program ending.")
 
